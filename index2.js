0675c52074818f12c21c056c9d32551f54574056
Kurtis Grant <kurtis.b.grant@gmail.com>
2021-12-03 15:21:50 +0000

    Finished index2 & iss_promised files using promises

--- index2.js
+++ index2.js
@@ -1,17 +1,15 @@
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss_promised');
+const { nextISSTimesForMyLocation } = require('./iss_promised');
 
-fetchMyIP()
-  .then(body => JSON.parse(body).ip)
-  .then(IP => fetchCoordsByIP(IP))
-  .then(body => JSON.parse(body))
-  .then(data => {
-    return { lat: data.latitude, lon: data.longitude };
+const printPassTimes = function(passTimes) {
+  for (passTime of passTimes) {
+    console.log(passTime);
+  }
+};
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
   })
-  .then(coords => fetchISSFlyOverTimes(coords))
-  .then(data => JSON.parse(data).response)
-  .then(data => data.map((line) => {
-    const date = new Date(line.risetime * 1000);
-    return `The ISS will pass at ${date.toLocaleTimeString()} on ${date.toLocaleDateString()} for ${line.duration} seconds!`;
-  }))
-  .then(results => console.log(results))
-  .catch(err => Error(err));
+  .catch(error => {
+    console.log("An error occurred: ", error.message);
+  });
--- iss_promised.js
+++ iss_promised.js
@@ -12,4 +12,22 @@ const fetchISSFlyOverTimes = function(coords) {
   return request(`https://iss-pass.herokuapp.com/json/?lat=${coords.lat}&lon=${coords.lon}`);
 };
 
-module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+    .then(body => JSON.parse(body).ip)
+    .then(IP => fetchCoordsByIP(IP))
+    .then(body => {
+      const data = JSON.parse(body);
+      return { lat: data.latitude, lon: data.longitude };
+    })
+    .then(coords => fetchISSFlyOverTimes(coords))
+    .then(body => {
+      const data = JSON.parse(body).response;
+      return data.map(dataLn => {
+        const riseTime = new Date(dataLn.risetime * 1000);
+        return `The ISS will pass at ${riseTime.toLocaleTimeString()} on ${riseTime.toLocaleDateString()} for ${dataLn.duration} seconds!`;
+      });
+    });
+};
+
+module.exports = { nextISSTimesForMyLocation };
